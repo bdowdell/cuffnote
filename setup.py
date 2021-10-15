81f933aa0286562011c5105c80c6d0d45a0eb0a2
Ben Dowdell <ben.dowdell87@gmail.com>
2021-10-15 15:20:34 -0500

    same

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup
 
 setup(
     name='cuffnote',
-    version=0.1.1,
+    version=0.2,
     description='Modeling mortgage loan scenarios',
     url='https://github.com/bdowdell/cuffnote',
     author='Ben Dowdell',
