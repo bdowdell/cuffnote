c8320cd918e3525a9b075f1f36636814cc265666
Ben Dowdell <ben.dowdell87@gmail.com>
2021-10-15 15:48:57 -0500

    increments version to v0.3.2

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup
 
 setup(
     name='cuffnote',
-    version='0.3.1',
+    version='0.3.2',
     description='Modeling mortgage loan scenarios',
     url='https://github.com/bdowdell/cuffnote',
     author='Ben Dowdell',
