3deefc7c59a26198d75e9eb1c4c474c55b6b7eef
Ben Dowdell <ben.dowdell87@gmail.com>
2021-10-15 15:18:49 -0500

    minor updates to pass CI/CD and publish

--- cuffnote/mortgages.py
+++ cuffnote/mortgages.py
@@ -1,9 +1,5 @@
 #!/usr/bin/env python
 
-# author: Ben Dowdell
-# email: ben.dowdell87@gmail.com
-# date of initial release: October 15, 2021
-
 # -*-coding:utf-8 -*-
 
 import numpy as np
--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup
 
 setup(
     name='cuffnote',
-    version=0.1,
+    version=0.1.1,
     description='Modeling mortgage loan scenarios',
     url='https://github.com/bdowdell/cuffnote',
     author='Ben Dowdell',
