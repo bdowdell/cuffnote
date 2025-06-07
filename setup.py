ceac0bf8c8cc5fabe6921d3e88c7e6b47c0f88bf
Ben Dowdell <ben.dowdell87@gmail.com>
2025-06-06 23:04:41 -0500

    Bumps to v0.3.6

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup
 
 setup(
     name='cuffnote',
-    version='0.3.5',
+    version='0.3.6',
     description='Modeling mortgage loan scenarios',
     url='https://github.com/bdowdell/cuffnote',
     author='Ben Dowdell',
